c78429eb689b8db709ef25d72967382845f16f36
Shakil Ahmed Atik <shakilatik@gmail.com>
2021-10-02 00:04:18 +0600

    Stripe payment handler added

--- controllers/auth.js
+++ controllers/auth.js
@@ -93,7 +93,6 @@ export const logout = async (req, res) => {
 export const currentUser = async (req, res) => {
   try {
     const user = await User.findById(req.user._id).select('-password').exec()
-    console.log('CURRENT_USER', user)
     return res.json({ ok: true })
   } catch (err) {
     console.log(err)
